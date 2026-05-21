b22626f0be1d338b5d19a09b4b1d65c2abb92a2a
MadeleineGustafson <madeleinegu@gmail.com>
2026-05-21 16:49:00 +0200

    created all function names

---
+++ src/ToDoList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {}
+
+  function addTask() {}
+
+  function deleteTask() {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+  return <></>;
+}
+
+export default ToDoList;
