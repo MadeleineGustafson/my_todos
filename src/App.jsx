dd14fc93c68b8a8e4327f8a00e699390b8c83cb5
MadeleineGustafson <madeleinegu@gmail.com>
2026-05-25 18:17:06 +0200

    adding elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import ToDoList from "./ToDoList";
 
 function App() {
-  return <ToDoList></ToDoList>;
+  return <ToDoList />;
 }
 
 export default App;
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    "Eat Breakfast",
+    "Take a shower",
+    "Walk the dog",
+  ]);
   const [newTask, setNewTask] = useState("");
 
-  function handleInputChange(event) {}
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
 
   function addTask() {}
 
@@ -13,7 +19,28 @@ function ToDoList() {
   function moveTaskUp(index) {}
 
   function moveTaskDown(index) {}
-  return <></>;
+  return (
+    <div>
+      <h1>To-do-lost</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{tasks}</span>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 export default ToDoList;
