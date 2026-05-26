d024a189ad536ce9f46bde4445071a3b657a108e
MadeleineGustafson <madeleinegu@gmail.com>
2026-05-26 15:25:27 +0200

    styling

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
+
+import { RxCross1 } from "react-icons/rx";
+import styled from "styled-components";
+
 function ToDoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
@@ -42,29 +47,125 @@ function ToDoList() {
     }
   }
   return (
-    <div>
-      <h1>To-do-lost</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter a task"
-          value={newTask}
-          onChange={handleInputChange}
-        />
-        <button onClick={addTask}>Add</button>
-      </div>
-      <ol>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            <span className="text">{task}</span>
-            <button onClick={() => deleteTask(index)}>Delete</button>
-            <button onClick={() => moveTaskUp(index)}>Up</button>
-            <button onClick={() => moveTaskDown(index)}>Down</button>
-          </li>
-        ))}
-      </ol>
-    </div>
+    <BigDiv>
+      <Title>To dos</Title>
+      <ContainerDiv>
+        <SmallDiv>
+          <InputField
+            type="text"
+            placeholder="Enter a task"
+            value={newTask}
+            onChange={handleInputChange}
+          />
+          <RoundButton onClick={addTask}>Add</RoundButton>
+        </SmallDiv>
+        <Task>
+          <ol>
+            {tasks.map((task, index) => (
+              <List key={index}>
+                <TaskSpan>{task}</TaskSpan>
+
+                <OtherButton onClick={() => moveTaskUp(index)}>
+                  <UpIcon />
+                </OtherButton>
+                <OtherButton onClick={() => moveTaskDown(index)}>
+                  <DownIcon />
+                </OtherButton>
+                <OtherButton onClick={() => deleteTask(index)}>
+                  <DeleteIcon />
+                </OtherButton>
+              </List>
+            ))}
+          </ol>
+        </Task>
+      </ContainerDiv>
+    </BigDiv>
   );
 }
 
 export default ToDoList;
+
+const Title = styled.h1`
+  font-size: 2rem;
+
+  color: black;
+`;
+
+const BigDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SmallDiv = styled.div``;
+
+const Task = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const InputField = styled.input`
+  width: 300px;
+  border-radius: 10px;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const RoundButton = styled.button`
+  border-radius: 20px;
+  padding: 10px;
+  border: none;
+  margin: 1rem;
+  color: white;
+  background: black;
+  font-weight: bold;
+`;
+
+const OtherButton = styled.button`
+  background: none;
+  border: none;
+`;
+
+const TaskSpan = styled.span`
+  flex: 1;
+  word-break: break-word;
+`;
+
+const List = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+  padding-bottom: 1rem;
+`;
+
+const ContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DeleteIcon = styled(RxCross1)`
+  font-size: 1rem;
+
+  &:hover {
+    transform: translateY(-2px);
+  }
+`;
+
+const UpIcon = styled(GoChevronUp)`
+  font-size: 1.5rem;
+
+  &:hover {
+    transform: translateY(-2px);
+  }
+`;
+
+const DownIcon = styled(GoChevronDown)`
+  font-size: 1.5rem;
+
+  &:hover {
+    transform: translateY(2px);
+  }
+`;
